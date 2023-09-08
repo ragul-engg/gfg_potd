526925ad5f86f1a3f1c52b1dfda13b2dfa4f51d1
ragul-engg <ragulshanmugarajan21@gmail.com>
2023-09-08 16:59:37 +0530

    added level pair for tree node in utils

---
+++ src/utils/TreeNodeLevelPair.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class TreeNodeLevelPair {
+	public TreeNode node;
+	public int level;
+
+	public TreeNodeLevelPair(TreeNode node, int level) {
+		this.node = node;
+		this.level = level;
+	}
+}
