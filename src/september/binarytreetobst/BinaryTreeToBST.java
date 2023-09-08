f804e5b8809eb7379d16f686cc7b071743a181e2
ragul-engg <ragulshanmugarajan21@gmail.com>
2023-09-08 11:04:26 +0530

    completed 08/09/2023 potd

---
+++ src/september/binarytreetobst/BinaryTreeToBST.java
@@ -0,0 +1,40 @@
+package september.binarytreetobst;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import utils.TreeNode;
+
+public class BinaryTreeToBST {
+
+	void addDataByInorder(TreeNode root, List<Integer> arr) {
+		if (root != null) {
+			addDataByInorder(root.left, arr);
+			arr.add(root.data);
+			addDataByInorder(root.right, arr);
+		}
+	}
+
+	void insertInTree(TreeNode root, List<Integer> arr, int[] index) {
+		if (root != null) {
+			insertInTree(root.left, arr, index);
+			root.data = arr.get(index[0]);
+			index[0]++;
+			insertInTree(root.right, arr, index);
+		}
+	}
+
+	TreeNode binaryTreeToBST(TreeNode root) {
+		List<Integer> arr = new ArrayList<>();
+		int[] index = new int[1];
+		addDataByInorder(root, arr);
+		Collections.sort(arr);
+		insertInTree(root, arr, index);
+		return root;
+	}
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ src/utils/TreeNode.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class TreeNode {
+	public int data;
+	public TreeNode left, right;
+
+	public TreeNode(int data) {
+		this.data = data;
+		this.left = this.right = null;
+	}
+}
