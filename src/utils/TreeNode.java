752891a54536db0dddee202b543a539e18cf38c1
ragul-engg <ragulshanmugarajan21@gmail.com>
2023-09-08 16:49:25 +0530

    updated treenode in utils

--- src/utils/TreeNode.java
+++ src/utils/TreeNode.java
@@ -8,4 +8,15 @@ public class TreeNode {
 		this.data = data;
 		this.left = this.right = null;
 	}
+
+	public static TreeNode insert(int idx, int n, int[] arr) {
+		TreeNode root = null;
+		if (idx < n && arr[idx] != -1) {
+			root = new TreeNode(arr[idx]);
+			root.left = insert(2 * idx + 1, n, arr);
+			root.right = insert(2 * idx + 2, n, arr);
+		}
+		return root;
+	}
+
 }
